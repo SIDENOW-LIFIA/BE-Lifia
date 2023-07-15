8f8148ef0f71f90cbb02ab2be62e7d5c633a3726
Bellwin98 <jongseunghan@naver.com>
2023-07-15 18:50:24 +0900

    first commit

---
+++ src/main/java/com/sidenow/ComplexBoardApplication.java
@@ -0,0 +1,13 @@
+package com.sidenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidenow/ComplexBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
