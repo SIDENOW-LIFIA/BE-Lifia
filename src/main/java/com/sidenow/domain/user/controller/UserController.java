fa12b54733376400d62f2c4e8334d637a0702267
BellWin98 <jongseunghan@naver.com>
2023-07-22 11:42:23 +0900

    Feat: Controller, Repository 개발 및 개발노트 작성

--- src/main/java/com/sidenow/domain/user/controller/UserController.java
+++ src/main/java/com/sidenow/domain/user/controller/UserController.java
@@ -1,11 +1,17 @@
 package com.sidenow.domain.user.controller;
 
+import com.sidenow.domain.user.entity.User;
 import com.sidenow.domain.user.service.UserService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,4 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/users")
 public class UserController {
     private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(@RequestParam(required = false))
 }
