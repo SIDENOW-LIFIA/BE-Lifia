08ecdf361e5818db622a8ec64a6a61a0e73774f2
BellWin98 <jongseunghan@naver.com>
2023-08-10 16:02:39 +0900

    Feat: JWT관련 Handler 및 환경설정 추가

--- src/main/java/com/sidenow/global/config/jwt/JwtSecurityConfig.java
+++ src/main/java/com/sidenow/global/config/jwt/JwtSecurityConfig.java
@@ -1,4 +1,22 @@
 package com.sidenow.global.config.jwt;
 
-public class JwtSecurityConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Spring Security에서 JWT인증을 구현하는데 필요한 필터를 구성
+ */
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
