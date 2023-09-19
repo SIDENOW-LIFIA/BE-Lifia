483ac6d5053fb1586113f005463607b02f742999
BellWin98 <jongseunghan@naver.com>
2023-09-19 21:14:19 +0900

    [Docs] 불필요 파일 삭제

--- src/main/java/com/sidenow/domain/member/service/MemberKakaoService.java
+++
@@ -1,8 +0,0 @@
-package com.sidenow.domain.member.service;
-
-import com.google.gson.JsonObject;
-
-public interface MemberKakaoService {
-    JsonObject connectKakao(String reqURL, String token);
-    String getEmail(JsonObject memberInfo);
-}
--- src/main/java/com/sidenow/domain/member/service/MemberMainService.java
+++ src/main/java/com/sidenow/domain/member/service/MemberMainService.java
@@ -1,8 +1,5 @@
 package com.sidenow.domain.member.service;
 
-import com.sidenow.domain.member.entity.Member;
-import com.sidenow.domain.member.dto.MemberDto.CheckNicknameResponse;
-
 public interface MemberMainService {
 
 }
