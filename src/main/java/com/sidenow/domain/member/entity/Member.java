3e2a7a047157da661557535cfb5ec554b8f58219
BellWin98 <jongseunghan@naver.com>
2023-10-18 00:30:55 +0900

    Docs: apartment nullable: false로 수정

--- src/main/java/com/sidenow/domain/member/entity/Member.java
+++ src/main/java/com/sidenow/domain/member/entity/Member.java
@@ -37,7 +37,7 @@ public class Member {
     @Column(unique = true, length = 20, nullable = false)
     private String nickname;
 
-    @Column(length = 50)
+    @Column(nullable = false, length = 50)
     private String apartment;
 
     @Enumerated(EnumType.STRING)
