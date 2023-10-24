15f291618aa41585dff0a06493340288fe3fe70c
BellWin98 <jongseunghan@naver.com>
2023-10-24 20:55:45 +0900

    Docs: 접근 제어자 에러 조치

--- src/main/java/com/sidenow/domain/boardType/caution/board/exception/constant/CautionExceptionList.java
+++ src/main/java/com/sidenow/domain/boardType/caution/board/exception/constant/CautionExceptionList.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CautionExceptionList {
-    CAUTION_ID_NOT_FOUND_ERROR("CC0001", HttpStatus.NOT_FOUND, "해당 postId인 게시글이 존재하지 않습니다."),
-    CAUTION_MEMBER_NOT_FOUND_ERROR("CC0002", HttpStatus.NOT_FOUND, "해당 유저의 게시글이 존재하지 않습니다."),
-    CAUTION_LIKE_HISTORY_NOT_FOUND_ERROR("CC0002", HttpStatus.NOT_FOUND, "해당 유저의 게시글 좋아요 이력이 존재하지 않습니다.");
+    CAUTION_ID_NOT_FOUND_ERROR("C0001", HttpStatus.NOT_FOUND, "해당 postId인 게시글이 존재하지 않습니다."),
+    CAUTION_MEMBER_NOT_FOUND_ERROR("C0002", HttpStatus.NOT_FOUND, "해당 유저의 게시글이 존재하지 않습니다."),
+    CAUTION_LIKE_HISTORY_NOT_FOUND_ERROR("C0002", HttpStatus.NOT_FOUND, "해당 유저의 게시글 좋아요 이력이 존재하지 않습니다.");
 
     private final String errorCode;
     private final HttpStatus httpStatus;
