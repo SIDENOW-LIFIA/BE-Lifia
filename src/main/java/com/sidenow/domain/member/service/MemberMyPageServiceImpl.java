e67cacb78da50193414dd542fb00ee2054b12e44
BellWin98 <jongseunghan@naver.com>
2023-10-25 16:24:51 +0900

    Feat: 마이페이지 API 구현 시작

--- src/main/java/com/sidenow/domain/member/service/MemberMyPageServiceImpl.java
+++ src/main/java/com/sidenow/domain/member/service/MemberMyPageServiceImpl.java
@@ -1,4 +1,12 @@
 package com.sidenow.domain.member.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
 public class MemberMyPageServiceImpl {
+
 }
